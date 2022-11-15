df79c0da4c1d26c24081820dea60537c36c21c49
tianying <tianyingxie@nsl.cs.waseda.ac.jp>
2022-11-15 18:46:04 +0900

    VAE with FID for FL, added saving generated images and labels

---
+++ VAE/server.py
@@ -0,0 +1,214 @@
+import flwr as fl
+from typing import Dict, List, Tuple, Union, Optional
+from flwr.common import Metrics
+from models import VAE
+import matplotlib.pyplot as plt
+from flwr.common.typing import Scalar
+import torch
+import numpy as np
+from client import CifarClient
+from utils import load_data, test, load_partition
+from torch.utils.data import DataLoader
+from collections import OrderedDict
+
+
+class SaveModelStrategy(fl.server.strategy.FedAvg):
+    def aggregate_fit(
+        self,
+        server_round: int,
+        results: List[Tuple[fl.server.client_proxy.ClientProxy, fl.common.FitRes]],
+        failures: List[Union[Tuple[fl.server.client_proxy.ClientProxy, fl.common.FitRes], BaseException]],
+    ) -> Tuple[Optional[fl.common.Parameters], Dict[str, Scalar]]:
+
+        # Call aggregate_fit from base class (FedAvg) to aggregate parameters and metrics
+        aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
+
+        if aggregated_parameters is not None:
+            # Convert `Parameters` to `List[np.ndarray]`
+            aggregated_ndarrays: List[np.ndarray] = fl.common.parameters_to_ndarrays(aggregated_parameters)
+            if server_round % 2 == 0:
+                # Save aggregated_ndarrays
+                print(f"Saving round {server_round} aggregated_ndarrays...")
+                np.savez(f"metrics/FL_round-{server_round}-weights.npz", *aggregated_ndarrays)
+
+        return aggregated_parameters, aggregated_metrics
+
+
+def get_evaluate_fn(model: torch.nn.Module):
+    """Return an evaluation function for server-side evaluation."""
+
+    def evaluate(
+            server_round: int, parameters: fl.common.NDArrays, config: Dict[str, Scalar]
+                 ):
+
+        # model = Net()
+        # model = set_parameters(model, parameters)  # Update model with the latest parameters
+        my_device = ""
+
+        try:
+            if torch.backends.mps.is_built():
+                my_device = "mps"
+        except AttributeError:
+            if torch.cuda.is_available():
+                my_device = "cuda:0"
+            else:
+                my_device = "cpu"
+
+        params_dict = zip(model.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        model.load_state_dict(state_dict, strict=True)
+        model.eval()
+        model.to(my_device)
+        if server_round % 2 == 0:
+
+            dataset = ["mnist", "fashion-mnist", "cifar", "stl"]
+            _, testset = load_partition(2, dataset[3])
+
+            valLoader = DataLoader(testset, batch_size=64, shuffle=True)
+            images = next(iter(valLoader))
+            recon_images, _, _ = model(images[0].to(my_device))
+
+            predictions = recon_images[:16]
+            fig = plt.figure(figsize=(3, 3))
+            for i in range(predictions.shape[0]):
+                plt.subplot(4, 4, i + 1)
+                img = np.transpose(predictions[i, :, :, ].cpu().detach().numpy(), axes=[1, 2, 0])
+                # plt.imshow((img/np.amax(img)*255).astype(np.uint8))
+                plt.imshow(img)
+                plt.axis('off')
+                # tight_layout minimizes the overlap between 2 sub-plots
+            plt.savefig('FL_G_data/image_at_epoch_{:03d}.png'.format(server_round))
+            plt.close()
+
+
+        # dataset = config["dataset"]
+        dataset = "stl"
+        _, testset = load_data(dataset)
+        valLoader = DataLoader(testset, batch_size=64)
+
+        my_device = ""
+
+        try:
+            if torch.backends.mps.is_built():
+                my_device = "mps"
+        except AttributeError:
+            if torch.cuda.is_available():
+                my_device = "cuda:0"
+            else:
+                my_device = "cpu"
+
+        model.to(my_device)
+        print(f"Global validation starting with device: {my_device}...")
+        loss, fid = test(model, valLoader, device=my_device)
+        print(f"Global validation end with FID: {fid}")
+        return loss, {"fid": float(fid)}
+
+    return evaluate
+
+
+def generate_and_save_images(model, epoch, test_input):
+    predictions = model.decoder(model.fc3(test_input))
+    fig = plt.figure(figsize=(3, 3))
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        img = np.transpose(predictions[i, :, :, ].detach().numpy(), axes=[1, 2, 0])
+        # plt.imshow((img/np.amax(img)*255).astype(np.uint8))
+        plt.imshow(img)
+        plt.axis('off')
+        # tight_layout minimizes the overlap between 2 sub-plots
+    plt.savefig('G_data/image_at_epoch_{:03d}.png'.format(epoch))
+    plt.close()
+
+
+def fit_config(server_round: int) -> Dict[str, Scalar]:
+    """Return a configuration with static batch size and (local) epochs."""
+    dp = ["laplace", "gaussian"]
+    config = {
+        "local_epochs": 10,  # number of local epochs
+        "batch_size": 64,
+        "server_round": server_round,
+        "dp": dp
+    }
+    return config
+
+
+def evaluate_fig(server_round: int):
+    val_batch_size = 64
+    dataset = ["mnist", "fashion-mnist", "cifar", "stl"]
+    val_config = {
+        "val_batch_size": val_batch_size,
+        "server_round": server_round,
+        "dataset": dataset[3]
+    }
+
+    return val_config
+
+
+def client_fn(cid: str) -> CifarClient:
+    my_device = ""
+
+    try:
+        if torch.backends.mps.is_built():
+            my_device = "mps"
+    except AttributeError:
+        if torch.cuda.is_available():
+            my_device = "cuda:0"
+        else:
+            my_device = "cpu"
+
+    DEVICE = torch.device(my_device)
+
+    # net = Net().to(DEVICE)
+    # trainset, testset, _ = load_data()
+
+    dataset = ["mnist", "fashion-mnist", "cifar", "stl"]
+    trainset, testset = load_partition(4, dataset[3])
+    return CifarClient(cid, trainset, testset, device=DEVICE)
+
+
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    # Multiply accuracy of each client by number of examples used
+    fid = [m["fid"] for number, m in metrics]
+    fid = np.average(fid)
+    # Aggregate and return average fid
+    return {"fid": fid}
+
+
+def main():
+    num_client = 2
+
+    dataset_type = "stl"
+    model = VAE(dataset_type)
+
+    model_parameters = [val.cpu().numpy() for _, val in model.state_dict().items()]
+
+    strategy = SaveModelStrategy(
+        fraction_fit=1,
+        fraction_evaluate=1,
+        min_fit_clients=num_client,
+        min_available_clients=num_client,
+        min_evaluate_clients=2,
+        evaluate_fn=get_evaluate_fn(model),
+        on_fit_config_fn=fit_config,
+        on_evaluate_config_fn=evaluate_fig,
+        initial_parameters=fl.common.ndarrays_to_parameters(model_parameters),
+        evaluate_metrics_aggregation_fn=weighted_average
+    )
+    """
+    fl.server.start_server(
+        server_address="10.1.2.102:8080",
+        config=fl.server.ServerConfig(num_rounds=1),
+        strategy=strategy
+    )
+    """
+
+    fl.simulation.start_simulation(
+        client_fn=client_fn,
+        num_clients=num_client,
+        config=fl.server.ServerConfig(num_rounds=5),
+        strategy=strategy
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ VAE/val_test.py
@@ -0,0 +1,158 @@
+from models import VAE
+from utils import Laplace_mech, Gaussian_mech, load_data, calculate_fid
+import torch
+from torch.utils.data import DataLoader
+import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn.functional as F
+import torchvision
+import time
+
+my_device = ""
+
+try:
+    if torch.backends.mps.is_built():
+        my_device = "mps"
+except AttributeError:
+    if torch.cuda.is_available():
+        my_device = "cuda:0"
+    else:
+        my_device = "cpu"
+
+DEVICE = torch.device(my_device)
+
+
+def train(net, trainloader, testloader, epochs, dp: str = "", device: str = "cpu"):
+    """Train the network on the training set."""
+    # optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-4)
+
+    net.train()
+    LOSS = 0.0
+    fid = []
+    start_time = time.time()
+
+    for e in tqdm.tqdm(range(epochs)):
+        loop = tqdm.tqdm((trainloader), total=len(trainloader), leave=False)
+        #for j, (images, labels) in enumerate(trainloader):
+        for images, labels in loop:
+
+            optimizer.zero_grad()
+
+            if dp == "laplace":
+                images = Laplace_mech(images).float()
+            if dp == "gaussian":
+                images = Gaussian_mech(images).float()
+
+            images = images.to(device)
+            recon_images, mu, logvar = net(images)
+
+            # recon_loss = F.mse_loss(recon_images, images)
+            recon_loss = F.binary_cross_entropy(recon_images, images, reduction='sum')
+            kld_loss = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+            loss = recon_loss + 0.05 * kld_loss
+            LOSS += loss
+            loss.backward()
+            optimizer.step()
+
+            loop.set_description(f"Epoch [{e}/{epochs}]")
+            loop.set_postfix(loss=loss.item())
+        # fid_e = calculate_fid(images[:16].cpu().detach().numpy().reshape(16, -1),
+        #                      recon_images[:16].cpu().detach().numpy().reshape(16, -1))
+
+        val_images, val_labels = next(iter(testloader))
+        del testloader
+
+        recon_images, _, _ = net(val_images.to(device))
+
+        predictions = recon_images[:16]
+
+        # 下面是快速计算的FID、调用Inception V3模型来计算
+        torch_size = torchvision.transforms.Resize([299, 299])
+
+        images_resize = torch_size(recon_images).to(device)
+
+        fid_model = torchvision.models.inception_v3(weights=torchvision.models.Inception_V3_Weights.DEFAULT)
+        fid_model.to(device)
+
+        global features_out_hook
+        features_out_hook = []
+        hook1 = fid_model.dropout.register_forward_hook(layer_hook)
+        fid_model.eval()
+        fid_model(images_resize)
+        images_features = np.array(features_out_hook).squeeze()
+        hook1.remove()
+
+        features_out_hook = []
+        recon_images_resize = torch_size(recon_images).to(device)
+        hook2 = fid_model.dropout.register_forward_hook(layer_hook)
+        fid_model.eval()
+        fid_model(recon_images_resize)
+        recon_images_features = np.array(features_out_hook).squeeze()
+        hook2.remove()
+
+        del fid_model
+
+        fid.append(calculate_fid(images_features, recon_images_features))
+        fig = plt.figure(figsize=(3, 3))
+        for i in range(predictions.shape[0]):
+            plt.subplot(4, 4, i + 1)
+            img = np.transpose(predictions[i, :, :, ].cpu().detach().numpy(), axes=[1, 2, 0])
+            plt.imshow(img)
+            plt.axis('off')
+            # tight_layout minimizes the overlap between 2 sub-plots
+        plt.savefig('G_data/image_at_epoch_{:03d}_G.png'.format(e))
+        plt.close()
+
+        grand_truth_images = val_images[:16]
+        fig = plt.figure(figsize=(3, 3))
+        for i in range(predictions.shape[0]):
+            plt.subplot(4, 4, i + 1)
+            img = np.transpose(grand_truth_images[i, :, :, ].cpu().detach().numpy(), axes=[1, 2, 0])
+            plt.imshow(img)
+            plt.axis('off')
+            # tight_layout minimizes the overlap between 2 sub-plots
+        plt.savefig('G_data/image_at_epoch_{:03d}.png'.format(e))
+        plt.close()
+
+        label_names = ["airplane", "bird", "car", "cat", "deer", "dog", "horse", "monkey", "ship", "truck"]
+        val_labels = [label_names[i] for i in np.array(val_labels)]
+
+        np.savetxt("G_data/Labels/label_at_epoch_{:03d}.txt".format(e), val_labels, fmt="%s")
+
+    end_time = time.time()
+    train_time = end_time - start_time
+
+    values = {
+        "FID": fid,
+        "Train_time": train_time
+    }
+
+    print("Save central values ")
+    np.save(f"Results/values_central_{epochs}.npy", values)
+
+features_out_hook = []
+def layer_hook(module, inp, out):
+    features_out_hook.append(out.data.cpu().numpy())
+
+
+def imshow(img):
+    img = img / 2 + 0.5 # 逆正则化
+    np_img = img.numpy()
+    plt.imshow(np.transpose(np_img, (1, 2, 0)))
+    plt.show()
+
+
+if __name__ == "__main__":
+    dp = ""
+    dataset = ["mnist", "fashion-mnist", "cifar", "stl"]
+    trainset, testset = load_data(dataset[3])
+    print(DEVICE)
+    net = VAE(dataset[3]).to(DEVICE)
+
+    trainLoader = DataLoader(trainset, batch_size=64, shuffle=True)
+    testLoader = DataLoader(testset, batch_size=64, shuffle=True)
+
+    epoch = 100
+    results = train(net, trainLoader, testLoader, epoch, dp, DEVICE)
